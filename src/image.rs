5a1b15465789374f074843ca691d369410a36f0f
Pandabear314 <34547084+Pandabear314@users.noreply.github.com>
2019-06-01 08:03:24 -0400

    Added abitlity to change pixels in images

--- src/image.rs
+++ src/image.rs
@@ -11,36 +11,52 @@ pub struct Color
 pub struct Image
 {
     data: Vec<u8>,
-    pub width: i32,
-    pub height: i32,
+    pub width: usize,
+    pub height: usize,
 }
 
 // Implement Image methods
 impl Image
 {
-    pub fn new(pixels: Vec<Vec<Color>>) -> Image
-    {
-        // Determine size of image
-        let im_height: usize = pixels.len();
-        let im_width: usize = pixels[0].len();
+    // Define constants
+    const DEFAULT_ALPHA: u8 = 255 as u8;
+    const BYTES_PER_PIXEL: usize = 4;
 
-        // Flatten pixels and flip upside down
-        let mut flat_data: Vec<u8> = Vec::with_capacity(im_height * im_width);
-        for i_row in (0..im_height).rev()
+    // Define constructor
+    pub fn new(im_width: usize, im_height: usize, background_color: Color) -> Image
+    {
+        // Create flat image with only background color
+        let n_pixels = im_height * im_width;
+        let mut flat_data: Vec<u8> = Vec::with_capacity(n_pixels * Image::BYTES_PER_PIXEL);
+        for _ in 0..n_pixels
         {
-            for i_pixel in 0..im_width
-            {
-                flat_data.push(pixels[i_row][i_pixel].red);
-                flat_data.push(pixels[i_row][i_pixel].green);
-                flat_data.push(pixels[i_row][i_pixel].blue);
-                flat_data.push(255 as u8);                      // Add alpha value
-            }
+            flat_data.push(background_color.red);
+            flat_data.push(background_color.green);
+            flat_data.push(background_color.blue);
+            flat_data.push(Image::DEFAULT_ALPHA);
         }
 
         // Return image struct
-        return Image{data: flat_data, width: im_width as i32, height: im_height as i32};
+        return Image{data: flat_data, width: im_width, height: im_height};
     }
 
+    // Modify a pixel in the image
+    pub fn set_pixel(&mut self, x: usize, y: usize, color: Color)
+    {
+        // Determine offset into data
+        let inv_y: usize = self.height - y - 1;                 // Invert y coordinate as image is stored upside down
+
+        let offset: usize = 
+            (inv_y * self.width * Image::BYTES_PER_PIXEL)  +    // Ofset for full rows
+            (x * Image::BYTES_PER_PIXEL);                       // Ofset for partial row
+
+        // Modify data
+        self.data[offset + 0] = color.red;
+        self.data[offset + 1] = color.green;
+        self.data[offset + 2] = color.blue;
+    }
+
+    // Get a c_void pointer to the image data
     pub fn get_ptr(&self) -> *const std::ffi::c_void
     {
         return self.data.as_ptr() as *const std::ffi::c_void;
--- src/main.rs
+++ src/main.rs
@@ -8,16 +8,14 @@ fn main()
     let black_pixel = image::Color{red: 0, green: 0, blue: 0};
     let white_pixel = image::Color{red: 255, green: 255, blue: 255};
 
-    let pixels: image::Image = 
-        image::Image::new(vec![
-            vec![black_pixel, black_pixel, black_pixel, black_pixel, black_pixel, black_pixel],
-            vec![black_pixel, white_pixel, black_pixel, black_pixel, white_pixel, black_pixel],
-            vec![black_pixel, black_pixel, black_pixel, black_pixel, black_pixel, black_pixel],
-            vec![black_pixel, white_pixel, black_pixel, black_pixel, white_pixel, black_pixel],
-            vec![black_pixel, black_pixel, white_pixel, white_pixel, black_pixel, black_pixel],
-            vec![black_pixel, black_pixel, black_pixel, black_pixel, black_pixel, black_pixel]
-        ]);
+    let mut pixels: image::Image = image::Image::new(6, 6, black_pixel);
+    pixels.set_pixel(1, 1, white_pixel);
+    pixels.set_pixel(4, 1, white_pixel);
+    pixels.set_pixel(1, 3, white_pixel);
+    pixels.set_pixel(2, 4, white_pixel);
+    pixels.set_pixel(3, 4, white_pixel);
+    pixels.set_pixel(4, 3, white_pixel);
 
     // Create new window
-    window::create_window(1600.0, 900.0, pixels.width, pixels.height, pixels.get_ptr());
+    window::create_window(1600.0, 900.0, pixels.width as i32, pixels.height as i32, pixels.get_ptr());
 }
